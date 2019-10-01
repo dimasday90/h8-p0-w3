68fab8d07c45984ab27d5c5884aa95e6b9b43d88
Dimas Agung Yudono <dimasday90@gmail.com>
2019-10-01 21:48:49 +0700

    add exercise 5

---
+++ exercise-05-palindrome.js
@@ -0,0 +1,18 @@
+function palindrome(kata) {
+    var kata2 = kata.split('').reverse().join('')
+    if(kata == kata2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
+
+console.log(palindrome('rusak kasur'));
+console.log(palindrome('klimak'));
+console.log(palindrome('kayak'));
